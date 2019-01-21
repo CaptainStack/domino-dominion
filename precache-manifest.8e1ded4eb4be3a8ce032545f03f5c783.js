dfb1a87d63268c2bc591d84924e3f1beb63673ab
CaptainStack <captainstack@gmail.com>
2019-01-20 20:26:25 -0800

    Updates

---
+++ precache-manifest.8e1ded4eb4be3a8ce032545f03f5c783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4f3d0a3f79489697ef9",
+    "url": "/domino-dominion/static/css/main.6070d345.chunk.css"
+  },
+  {
+    "revision": "a4f3d0a3f79489697ef9",
+    "url": "/domino-dominion/static/js/main.a4f3d0a3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/domino-dominion/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "aef3e7e3020c01ec64d3",
+    "url": "/domino-dominion/static/js/runtime~main.aef3e7e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/domino-dominion/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cff7d7b61961834cbc7272c863a507b0",
+    "url": "/domino-dominion/index.html"
+  }
+];
