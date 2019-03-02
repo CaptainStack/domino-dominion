26980d7a2db39a6f838c9138f2d62f88650de46c
Andre Stackhouse <captainstack@gmail.com>
2019-03-02 15:16:11 -0800

    Add legal adjacency into event system.

--- src/events.js
+++ src/events.js
@@ -1,9 +1,12 @@
 import { store } from './index';
+import { get_occupied_adjacent_tiles } from './initial_state'
 
 export const newGameButtonClicked = event => {
   store.dispatch({ type: 'INITIALIZE_BOARD' });
 }
 
 export const tileClicked = clicked_tile => event => {
-  store.dispatch({ type: 'ADVANCE_CONTENT', tile: clicked_tile });
+  if (get_occupied_adjacent_tiles(store.getState().board, clicked_tile).length > 0) {
+    store.dispatch({ type: 'ADVANCE_CONTENT', tile: clicked_tile });
+  }
 }
